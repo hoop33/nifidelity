2bced8e0fcf7b389a7a10e39aac6e1d0df4419bb
Rob Warner <rwarner@grailbox.com>
2021-02-03 11:16:50 -0500

    feat: read mermaid template from file

--- bin/index.js
+++ bin/index.js
@@ -9,6 +9,10 @@ program.version(pckg.version);
 program.option("-i, --input <directory>", "input directory", ".");
 program.option("-o, --output <directory>", "output directory", ".");
 program.option(
+  "-m, --mermaid <template_file>",
+  "file containing mermaid template for flow diagrams (use CONTENTS in template for content placement)"
+);
+program.option(
   "-O, --overwrite",
   "overwrite files in the output directory",
   false
