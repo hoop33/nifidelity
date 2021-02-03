8ce00993c7503a36a7f4ef7820b2c867edec57fe
Rob Warner <rwarner@grailbox.com>
2021-02-03 13:55:38 -0500

    feat: load rules from file

--- bin/index.js
+++ bin/index.js
@@ -12,10 +12,37 @@ program.option(
   "-m, --mermaid <template_file>",
   "file containing mermaid template for flow diagrams (use CONTENTS in template for content placement)"
 );
+program.option("-r, --rules <rules_file>", "rules file in Validate.js format");
 program.option(
   "-O, --overwrite",
   "overwrite files in the output directory",
   false
+);
+
+program.addHelpText(
+  "after",
+  `
+
+Example rules file:
+{
+  "bucket": {
+    "bucketId": "required|alpha_dash",
+    "path": "required|string"
+  },
+  "flow": {
+    "name": "required|string|min:4",
+    "description": "required|string|min:6",
+    "comments": "required|string|min:6",
+    "contents.content.flowSnapshot.flowContents.comments":
+      "required|string|min:6",
+  },
+}
+
+Example mermaid template file:
+import Mermaid from '@theme/Mermaid';
+
+<Mermaid chart={\`CONTENTS\`}/>
+`
 );
 
 program.parse(process.argv);
